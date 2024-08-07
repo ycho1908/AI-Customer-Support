7172765ffe2cef755753ed1c17910cff300e809e
Evelyn Cho <yehyeoncho@gmail.com>
2024-08-07 17:25:38 +0900

    implemented chat display

---
+++ app/page.js
@@ -0,0 +1,108 @@
+'use client'
+import { Box, Button, Stack, TextField } from '@mui/material'
+import Image from "next/image"
+import { useState } from 'react'
+
+
+export default function Home() {
+  const [messageHistory, setMessageHistory] = useState([{
+    role: 'assistant',
+    content: `Hi I'm the Headstarter Support Agent, how can I assist you today?`,
+  }])
+
+  const [message, setMessage] = useState('')
+
+  const sendMessage = async () => {
+    setMessage('')
+    setMessageHistory((messageHistory) => [
+      ...messageHistory,
+      {role: 'user', content: message},
+      {role: 'asssistant', content: ''},
+    ])
+    const response = fetch('/api/chat', {
+      method: Postpone,
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify([...message, {role: 'user', content: message}]),
+    }).then(async (res) => {
+      const reader = res.body.getReader()
+      const decoder = new TextDecoder()
+
+      let result = ''
+      return reader.read().then(function processText({done, value}) {
+        if (done) {
+          return result
+        }
+        const text = decoder.decode(value || new Int8Array(), {stream: true})
+        setMessageHistory((messageHistory) => {
+          let lastMessage = messageHistory[messageHistory.length-1]
+          let otherMessages = messageHistory.slice(0,messageHistory.length-1)
+          return[
+            ...otherMessages,
+            {
+              ...lastMessage,
+              content: lastMessage.content + text,
+            },
+          ]
+        })
+        return reader.read().then(processText)
+      })
+    })
+  }
+
+  return (
+    <Box
+      width='100vw'
+      height='100vh'
+      display='flex'
+      flexDirection='column'
+      justifyContent='center'
+      alignItems='center'
+    >
+      <Stack
+        direction='column'
+        width='600px'
+        height='700px'
+        border='1px solid black'
+        p={2}
+        spacing={2}
+      >
+        <Stack
+          direction='column'
+          spacing={2}
+          flexGrow={1}
+          overflow='auto'
+          maxHeight='100%'
+        >
+        {
+          messageHistory.map((message, index) => (
+            <Box
+              key={index}
+              display={'flex'}
+              justifyContent={message.role === 'assistant' ? 'flex-start' : 'flex-end'}
+            >
+              <Box
+                bgcolor={message.role === 'assistant' ? 'primary.main' : 'secondary.main'}
+                color={'white'}
+                borderRadius={10}
+                p={2}
+              >
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction='row' spacing={2}>
+          <TextField
+            label='message'
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant='contained' onClick={sendMessage}>Send</Button>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
